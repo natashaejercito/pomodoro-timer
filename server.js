b611a5d3f69bfffbf5f826ecd4da0b7d5a6517f9
Natasha Nicole Ejercito <natasha.ejercito@outlook.com>
2026-01-11 20:53:59 -0500

    added react file

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
 //paths
+const express = require("express");
+const app = express();
+const page1 = require("./homePage.js");
+
+const HTTP_PORT = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+  app.send({ page1 });
+});
+
+app.listen(HTTP_PORT, () => {
+  console.log(`server is running in: http://localhost:${HTTP_PORT}`);
+});
