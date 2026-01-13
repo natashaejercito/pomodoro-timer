a079411d69c7524518cba417d81877eaf7a43454
Natasha Nicole Ejercito <natasha.ejercito@outlook.com>
2026-01-13 14:41:33 -0500

    added timer page

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import TimerPage from "./TimerPage";
 
 const HomePage = () => {
   return (
     <div>
       <h1>Welcome to Pomodoro Timer!</h1>
       <button>Start</button>
+      <TimerPage />
     </div>
   );
 };
---
+++ frontend/src/components/TimerPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const TimerPage = () => {
+  return (
+    <div>
+      <h1>Time</h1>
+      <button>Home</button>
+      <button>Start</button>
+      <button>Stop</button>
+      <button>Restart</button>
+    </div>
+  );
+};
+
+export default TimerPage;
